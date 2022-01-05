cd01baaa8af30ebbff6a60c877ac2e2e461364bf
murtagh95 <nec.catalano@gmail.com>
2022-01-05 12:34:41 -0300

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <main>
     <px-header></px-header>
-    <px-assets-table></px-assets-table>
+    <router-view class="container px-5 sm:px-20 py-20 flex justify-center" />
   </main>
 </template>
 
 <script>
-import PxAssetsTable from "@/components/PxAssetsTable";
 import PxHeader from "@/components/PxHeader.vue";
 
 export default {
   name: "App",
   components: {
     PxHeader,
-    PxAssetsTable,
   },
 };
 </script>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyexto para obtener las cotizaciones de las cruptomonedas más
+      importantes a traves de la API REST de Coincap.
+      <br />Este proyecto es utilizado en el curso de Vue.js Basico de Plazi
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver al home
+    </router-link>
+  </div>
+</template>
