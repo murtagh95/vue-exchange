35db68cb61679069280b1458b5027f6cfc2ef3ef
murtagh95 <nec.catalano@gmail.com>
2022-01-05 16:53:11 -0300

    feat: add button component

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div class="flex-col items-center">
     <h1 class="text-gray-700 text-6xl">Exchange v1.0.0</h1>
     <p class="text-gray-600 text-xl text-center">
-      Proyexto para obtener las cotizaciones de las cruptomonedas más
+      Proyecto para obtener las cotizaciones de las cruptomonedas más
       importantes a traves de la API REST de Coincap.
       <br />Este proyecto es utilizado en el curso de Vue.js Basico de Plazi
     </p>
